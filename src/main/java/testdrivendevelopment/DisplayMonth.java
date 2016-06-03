cdee7adf733780536eb32bfce5ba0d317aa35eb6
Jay <j.sarna@clear.net.nz>
2016-06-03 19:00:23 +1200

    test

--- src/main/java/testdrivendevelopment/DisplayMonth.java
+++ src/main/java/testdrivendevelopment/DisplayMonth.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class DisplayMonth
  */
-
+@WebServlet("/DisplayMonth")
 public class DisplayMonth extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
