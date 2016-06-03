772b163fbe6713d203e0f2d154c6f939c5bd1db7
Jay <j.sarna@clear.net.nz>
2016-06-03 18:45:22 +1200

    test

--- src/main/java/testdrivendevelopment/DisplayMonth.java
+++ src/main/java/testdrivendevelopment/DisplayMonth.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class DisplayMonth
  */
+@WebServlet(name="DisplayMonth",urlPatterns={"/DisplayMonth"})
 public class DisplayMonth extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
